3083476d32c8caaa8b73c9712d17261284a030ff
fawzy <ma7madfawzy@gmail.com>
2019-07-17 15:31:02 +0200

    bug fixing

--- barchart/src/main/java/com/widget/barchart/chart/BarsAdapter.java
+++ barchart/src/main/java/com/widget/barchart/chart/BarsAdapter.java
@@ -67,6 +67,7 @@ public class BarsAdapter extends RecyclerView.Adapter<BarsAdapter.ViewHolder> {
 
         public void bind(int position) {
             binding.title.setTextColor(columnLabelColor);
+            binding.title.setSelected(true);
             binding.setColumnColor(columnColor);
             binding.setColumnBG(columnBG);
             this.binding.setModel(dataList.get(position));
